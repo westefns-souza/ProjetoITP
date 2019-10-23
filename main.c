da20bb7553dac294e583aefa9bac0b4fb1e930d5
WESTEFNS DAMIAO DE SOUZA SILVA <westefns@A307-14L.imd.local>
2019-10-23 20:24:26 -0300

    Definição de structs, enums e inicio da leitura e escrita de arquivos.

--- main.c
+++ main.c
@@ -1,7 +1,88 @@
 #include <stdio.h>
 
+typedef struct image_t
+{
+    int largura;
+    int altura;
+} image;
+
+
+typedef struct color_t
+{
+    int r;
+    int g;
+    int b;
+} color;
+
+typedef struct coordenadas_t
+{
+    int x;
+    int y;
+} coordenadas;
+
+typedef struct linha_t
+{
+    coordenadas cordenada1;
+
+    coordenadas cordenada2;
+} linha;
+
+typedef struct polygon_t
+{
+    int quantidade;
+    
+    coordenadas *cordenadas;
+} polygon;
+
+typedef struct rect_t
+{
+    coordenadas cordenadas;
+    
+    int altura;
+    int largura;
+} rect;
+
+typedef struct circle_t
+{
+    coordenadas cordenadas;
+    
+    int raio;
+} circle;
+
+typedef struct fill_t
+{
+    coordenadas cordenadas;
+} fill;
+
+enum operacoes 
+{
+    Save = 1,
+    Open
+} operacoes;
+
 int main()
 {
+    FILE* arquivo;
+    int i = 0;
+    
+    arquivo = fopen("arquivo_de_especificacao.txt", "r");
+    
+    while(!feof(arquivo))
+    {
+        char linha[100];
+        char *result;
+        
+        result = fgets(linha, 100, arquivo);
+
+        if (result)
+        {
+            printf("Linha %d : %s\n", i, linha);
+        }
+
+        i++;
+    }
+
+    fclose(arquivo);
 
     return 0;
 }
