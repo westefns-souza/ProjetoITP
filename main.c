704c2e2882da03dcbc5e16ede42caa93ab3d1f7a
WESTEFNS DAMIAO DE SOUZA SILVA <westefns@A307-09L.imd.local>
2019-10-25 20:28:30 -0300

    Primeiro Check-point

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct image_t
 {
@@ -57,16 +59,24 @@ typedef struct fill_t
 enum operacoes 
 {
     Save = 1,
-    Open
+    Open,
+    Image
 } operacoes;
 
 int main()
 {
     FILE* arquivo;
-    int i = 0;
-    
+    FILE* arquivo_ppm;
+    FILE *save;
+    FILE *aux;
+
+    int operacao;
+
     arquivo = fopen("arquivo_de_especificacao.txt", "r");
-    
+    arquivo_ppm = fopen("arquivo_auxiliar.ppm", "w+");
+
+    fprintf(arquivo_ppm, "P3\n");
+
     while(!feof(arquivo))
     {
         char linha[100];
@@ -76,12 +86,57 @@ int main()
 
         if (result)
         {
-            printf("Linha %d : %s\n", i, linha);
+            char *token;
+
+            token = strtok(linha, " ");
+
+            operacao = strcmp(token, "image");
+
+            if (operacao == 0)
+            {
+                image imagem;
+
+                imagem.largura = atoi(strtok(NULL, " "));
+                imagem.altura = atoi(strtok(NULL, " "));
+
+               fprintf(arquivo_ppm, "%d %d\n", imagem.largura, imagem.altura);
+
+               continue;
+            }
+
+            operacao = strcmp(token, "save");
+
+            if (operacao == 0)
+            {
+                char *nome_do_arquivo = strtok(NULL, " ");
+
+                save = fopen(nome_do_arquivo, "w+");
+
+                fclose(arquivo_ppm);
+
+                aux = fopen("arquivo_auxiliar.ppm", "r");
+                
+                char linha_ppm[100];
+                char *result_ppm;
+                while(!feof(aux))
+                {
+        
+                    result_ppm = fgets(linha_ppm, 100, aux);
+                    
+                    if (result_ppm)
+                    {
+                        fprintf(save, "%s", linha_ppm);
+                    }
+                }
+
+                fclose(aux);
+
+               continue;
+            }
         }
-
-        i++;
     }
-
+    
+    fclose(save);
     fclose(arquivo);
 
     return 0;
